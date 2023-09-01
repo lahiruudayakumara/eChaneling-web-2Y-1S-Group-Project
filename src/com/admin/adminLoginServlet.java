1eb8f3bea56e8ee01af4e639bb496901bc5fabc0
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-09-01 14:29:32 +0530

    Create adminLoginServlet.java

---
+++ src/com/admin/adminLoginServlet.java
@@ -0,0 +1,40 @@
+package com.admin;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class adminLoginServlet
+ */
+@WebServlet("/adminLoginServlet")
+public class adminLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userName = request.getParameter("uid");
+		String password = request.getParameter("pass");
+		
+		try {
+			List<Admin> adminDetails = AdminDBUtil.validate(userName, password);
+			request.setAttribute("adminDetails", adminDetails);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher dis = request.getRequestDispatcher("admin_dashboard.jsp");
+		dis.forward(request, response);
+	}
+
+}
