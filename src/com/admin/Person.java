a29d4d310b9fb8d7923b753d96382285b6c449cf
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-09-28 18:00:46 +0530

    Create Person.java
    This class is Parent class in admin and doctor.

---
+++ src/com/admin/Person.java
@@ -0,0 +1,39 @@
+package com.admin;
+
+class Person {
+	
+	protected int id;
+	protected String fullName;
+	protected String email;
+	protected String phoneNo;
+	protected String password;
+	
+	public Person(int id, String fullName, String email, String phoneNo, String password) {
+		this.id = id;
+		this.fullName = fullName;
+		this.email = email;
+		this.phoneNo = phoneNo;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+}
