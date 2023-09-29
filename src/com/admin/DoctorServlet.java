bccfc989e11a805ac05493d3d99dc91f587be87d
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-09-29 19:14:20 +0530

    Create DoctorServlet.java
    This servlet use retrieve doctor details

---
+++ src/com/admin/DoctorServlet.java
@@ -0,0 +1,29 @@
+package com.admin;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DoctorServlet
+ */
+@WebServlet("/DoctorServlet")
+public class DoctorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Inside your servlet's doGet method
+		List<Doctor> doctorList = DoctorDBUtil.getDoctorDetails();
+		request.setAttribute("doctorList", doctorList);
+		request.getRequestDispatcher("doctorDetails.jsp").forward(request, response);
+	}
+}
