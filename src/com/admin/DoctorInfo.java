7a7c9635f6b5bce9928b6dd5af46736f4464b5f9
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-10-02 15:25:46 +0530

    Update DoctorInfo.java
    bug fix and improve  code

--- src/com/admin/DoctorInfo.java
+++ src/com/admin/DoctorInfo.java
@@ -20,12 +20,11 @@ public class DoctorInfo extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String id = request.getParameter("docId");
 		int cid = Integer.parseInt(id);
-		boolean isTrue;
-
+		
 		HttpSession session = request.getSession(false);
 		
 		if (session != null) {
@@ -39,6 +38,9 @@ public class DoctorInfo extends HttpServlet {
         } else {
         	response.sendRedirect("admin_login.jsp");
         }
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
 
 }
