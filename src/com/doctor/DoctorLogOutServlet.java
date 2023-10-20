dcb3d6be39f04052f33381bed2b6a89d6358d1cf
Dushan Dhananjaya <128561812+IT22327208@users.noreply.github.com>
2023-10-21 04:15:10 +0530

    update DBUtil and servlet files
    commit update DBUtil and servlet files

---
+++ src/com/doctor/DoctorLogOutServlet.java
@@ -0,0 +1,24 @@
+package com.doctor;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DoctorLogOutServlet
+ */
+@WebServlet("/DoctorLogOutServlet")
+public class DoctorLogOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
