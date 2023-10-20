a454bcb9695d5a92be02df536b1bf2c7f6423108
Dushan Dhananjaya <128561812+IT22327208@users.noreply.github.com>
2023-10-21 04:27:58 +0530

    create doctor log out
    commit create doctor log out

--- src/com/doctor/DoctorLogOutServlet.java
+++ src/com/doctor/DoctorLogOutServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class DoctorLogOutServlet
@@ -18,7 +19,14 @@ public class DoctorLogOutServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		HttpSession session = request.getSession();
+        
+        if (session.getAttribute("email") != null) {
+        	session.invalidate();
+        	response.sendRedirect("doctorLogin.jsp");
+        } else {
+        	response.getWriter().println("You are not logged in");
+        }
 	}
 
 }
