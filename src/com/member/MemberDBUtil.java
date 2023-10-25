77f0ad713fba09a355f8263e5b9369802b4aac5e
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-10-25 15:24:33 +0530

    add MemberDBUtil

--- src/com/member/MemberDBUtil.java
+++ src/com/member/MemberDBUtil.java
@@ -8,20 +8,20 @@ import java.util.List;
 
 public class MemberDBUtil {
 	
+	public static  boolean isSuccess;
+	public static 	String sql;
 	private static Connection con = null;
 	private static Statement stmt = null;
 	private static ResultSet rs = null;
 	
 	public static boolean dropMessage(String name, String email, String phone, String subject, String message, String userName) {
-		
-		boolean isSuccess = false;
-		
+			
 		try {
 			// Establish a database connection
 			con = DBConnect.getConnection();
 			stmt = con.createStatement();
 			
-			String sql = "INSERT INTO contact_us VALUES(0,'"+ name+"','"+email+"','"+phone+"','"+subject+"','"+message+"','"+userName+"')";
+			sql = "INSERT INTO contact_us VALUES(0,'"+ name+"','"+email+"','"+phone+"','"+subject+"','"+message+"','"+userName+"')";
 			
 			int result = stmt.executeUpdate(sql);
 			
@@ -31,6 +31,63 @@ public class MemberDBUtil {
 			else {
 				isSuccess = false;
 			}	
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return isSuccess;
+	}
+	
+	public static boolean updateMessage(String id, String name, String email, String phone, String subject, String message) {
+		
+		int convId = Integer.parseInt(id);
+		
+		try {
+			
+			// Establish a database connection
+			con = DBConnect.getConnection();
+			stmt = con.createStatement();
+			
+			sql = "UPDATE contact_us SET name='"+name+"',email='"+email+"',phone='"+phone+"',subject='"+subject+"',message='"+message+"' WHERE messageId='"+convId+"'";
+			
+			int result = stmt.executeUpdate(sql);
+			
+			if(result > 0) {
+				isSuccess=true;
+			}
+			else {
+				isSuccess=false;
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return isSuccess;
+	}
+	
+	public static boolean deleteMessage(String id) {
+		
+		
+		int convId = Integer.parseInt(id);
+		
+		try {
+			
+			con = DBConnect.getConnection();
+			stmt = con.createStatement();
+			
+			sql = "DELETE FROM contact_us WHERE messageId='"+convId+"'";
+			
+			int result = stmt.executeUpdate(sql);
+			
+			if(result > 0) {
+				isSuccess=true;
+			}
+			else {
+				isSuccess=false;
+			}
+			
 		}
 		catch(Exception e) {
 			e.printStackTrace();
